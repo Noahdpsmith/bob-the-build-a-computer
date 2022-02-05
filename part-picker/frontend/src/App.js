68a0cf807ce1e46efb6184826a6c750e737a31f4
mjlars <maflarson@gmail.com>
2022-02-05 16:19:32 -0700

    built the main routes, need to do the individual component routes

--- part-picker/frontend/src/App.js
+++ part-picker/frontend/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import About from './components/About';
-import Newbuild from './components/Newbuild';
-import MyBuilds from './components/MyBuilds';
-import Parts from './components/Parts';
-import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Homepage from './components/Homepage.js';
+import About from './components/About.js';
+import Newbuild from './components/Newbuild.js';
+import MyBuilds from './components/MyBuilds.js';
+import Parts from './components/Parts.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
@@ -17,30 +16,31 @@ function App() {
         <Route exact path='/about' component={About} />
         <Route exact path='/newbuild' component={Newbuild} />
         <Route exact path='/mybuilds' component={MyBuilds} />
-        {/* <Route exact path='/parts' component={Parts} /> */}
-        <Route
-          path="/parts"
-          render={({ match: { url } }) => (
-            <>
-              <Route path={`${url}/`} component={Parts} exact/>
-              <Route path={`${url}/cpu`} component={Cpu} />
-              <Route path={`${url}/gpu`} component={Gpu} />
-              <Route path={`${url}/ram`} component={Ram} />
-              <Route path={`${url}/motherboard`} component={Motherboard} />
-                {/* note: storage is spelled with a '0' rather than an 'o' to prevent react from trying to implement localstorage or other types of storage */}
-                {/* storage component will be for hdd and ssd */}
-              <Route path={`${url}/storage`} component={St0rage} />
-              <Route path={`${url}/psu`} component={Psu} />
-              <Route path={`${url}/case`} component={Case} />
-              <Route path={`${url}/cooler`} component={Cooler} />
-              <Route path={`${url}/accessories`} component={Accessories} />
-              <Route path={`${url}/fan`} component={Fan} />
-            </>
-          )}
-        />
+        <Route exact path='/parts' component={Parts} />
       </Switch>
     </Router>
   );
 }
 
 export default App;
+
+        // <Route
+        //   path="/parts"
+        //   render={({ match: { url } }) => (
+        //     <>
+        //       <Route path={`${url}/`} component={Parts} exact/>
+        //       <Route path={`${url}/cpu`} component={Cpu} />
+        //       <Route path={`${url}/gpu`} component={Gpu} />
+        //       <Route path={`${url}/ram`} component={Ram} />
+        //       <Route path={`${url}/motherboard`} component={Motherboard} />
+        //         {/* note: storage is spelled with a '0' rather than an 'o' to prevent react from trying to implement localstorage or other types of storage */}
+        //         {/* storage component will be for hdd and ssd */}
+        //       <Route path={`${url}/storage`} component={St0rage} />
+        //       <Route path={`${url}/psu`} component={Psu} />
+        //       <Route path={`${url}/case`} component={Case} />
+        //       <Route path={`${url}/cooler`} component={Cooler} />
+        //       <Route path={`${url}/accessories`} component={Accessories} />
+        //       <Route path={`${url}/fan`} component={Fan} />
+        //     </>
+        //   )}
+        // />
---
+++ part-picker/frontend/src/components/Parts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Parts = () => {
+
+    return(
+        <div>
+            <h1>
+                Parts
+            </h1>
+        </div>
+    );
+}
+
+
+
+export default Parts;
