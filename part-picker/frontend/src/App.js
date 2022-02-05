0ca6006ddc1cd215177261f3488f55d58f69ea2f
mjlars <maflarson@gmail.com>
2022-02-04 18:48:34 -0700

    got some of the routes done, just need to reformat them and add routes for individual components

--- part-picker/frontend/src/App.js
+++ part-picker/frontend/src/App.js
@@ -17,7 +17,27 @@ function App() {
         <Route exact path='/about' component={About} />
         <Route exact path='/newbuild' component={Newbuild} />
         <Route exact path='/mybuilds' component={MyBuilds} />
-        <Route exact path='/parts' component={Parts} />
+        {/* <Route exact path='/parts' component={Parts} /> */}
+        <Route
+          path="/parts"
+          render={({ match: { url } }) => (
+            <>
+              <Route path={`${url}/`} component={Parts} exact/>
+              <Route path={`${url}/cpu`} component={Cpu} />
+              <Route path={`${url}/gpu`} component={Gpu} />
+              <Route path={`${url}/ram`} component={Ram} />
+              <Route path={`${url}/motherboard`} component={Motherboard} />
+                {/* note: storage is spelled with a '0' rather than an 'o' to prevent react from trying to implement localstorage or other types of storage */}
+                {/* storage component will be for hdd and ssd */}
+              <Route path={`${url}/storage`} component={St0rage} />
+              <Route path={`${url}/psu`} component={Psu} />
+              <Route path={`${url}/case`} component={Case} />
+              <Route path={`${url}/cooler`} component={Cooler} />
+              <Route path={`${url}/accessories`} component={Accessories} />
+              <Route path={`${url}/fan`} component={Fan} />
+            </>
+          )}
+        />
       </Switch>
     </Router>
   );
