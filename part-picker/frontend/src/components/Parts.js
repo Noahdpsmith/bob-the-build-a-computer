427165c3b405c5ab6bb0526db867bd7219e1d0f1
mjlars <maflarson@gmail.com>
2022-02-12 17:50:22 -0700

    small change

--- part-picker/frontend/src/components/Parts.js
+++ part-picker/frontend/src/components/Parts.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
 const Parts = () => {
 
     return(
         <div>
-            <h1>
-                Parts
-            </h1>
+            
+               <Route path='/cpu' component={Cpu} />
+               <Route path='/ram' component={Ram} />
+               <Route path='/motherboard' component={Motherboard} />
+               <Route path='/gpu' component={Gpu} />
+                 {/* note: storage is spelled with a '0' rather than an 'o' to prevent react from trying to implement localstorage or other types of storage */}
+                 {/* storage component will be for hdd and ssd */}
+               <Route path='/storage' component={St0rage} />
+               <Route path='/psu' component={Psu} />
+               <Route path='/case' component={Case} />
+               <Route path='/cooler' component={Cooler} />
+               <Route path='/accessories' component={Accessories} />
+               <Route path='/fan' component={Fan} />
+
         </div>
     );
 }
