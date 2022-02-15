d67b5dcea27486a515246b189a41b5a0459a0c73
mjlars <maflarson@gmail.com>
2022-02-14 19:29:12 -0700

    sdf

--- part-picker/frontend/src/App.js
+++ part-picker/frontend/src/App.js
@@ -51,7 +51,7 @@ function App() {
             </>
           )}
         />
-               
+          <Route path='/parts/cpu/:id' component={IndividualCpu} />
         </Switch>
 
       <Footer/>
