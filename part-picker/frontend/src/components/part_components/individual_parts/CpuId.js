7823900a46bfb5af1c1bb8bfe8f8c660dcb86c32
mjlars <maflarson@gmail.com>
2022-02-14 20:09:35 -0700

    yee yee

---
+++ part-picker/frontend/src/components/part_components/individual_parts/CpuId.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, useParams, Route, Switch, useRouteMatch, Link } from 'react-router-dom';
+
+
+
+const CpuId = () => {
+
+    
+}
+
+export default CpuId;
